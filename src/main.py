5af3482b09265939336a37baa25559f20e29d2bb
zerondipity <zerondipity@gmail.com>
2021-06-13 01:32:59 +0800

    ga-net

---
+++ src/main.py
@@ -0,0 +1,42 @@
+import networkx as nx
+import time
+from numpy.lib.npyio import NpzFile
+from src.ga_net import ga_community_detection
+from src.util import loadDataset, modularity, transfer_cluster
+import numpy as np
+
+
+if __name__ == '__main__':
+    path ='./soc-karate/soc-karate.mtx'
+    mtx = loadDataset(path)
+
+    modularity_bst = 0
+    modularity_avg = 0
+    nfe_bst = 0
+    nfe_avg = 0
+
+    for _ in range(1):
+
+        G = nx.karate_club_graph()
+        cluster, nfe = ga_community_detection(G, population=30, generation=100, r = 1.5)
+        
+
+        if nfe_bst == 0 or nfe < nfe_bst:
+            nfe_bst = nfe
+        nfe_avg += nfe
+
+        cluster = transfer_cluster(cluster)
+
+        Q = modularity(cluster, mtx)
+
+        if Q > modularity_bst:
+            modularity_bst = Q
+        modularity_avg += Q
+
+    print('mod_bst: ' , modularity_bst)
+    print('mod_avg: ' , modularity_avg / 100)
+    print('nfe_bst: ' , nfe_bst)
+    print('nfe_avg: ' , nfe_avg / 100)
+
+
+
--- src/util.py
+++ src/util.py
@@ -1,6 +1,37 @@
 from scipy.io import mmread
-
+import numpy as np
 def loadDataset(path):
     mtx = mmread(path)
     mtx = mtx.todense()
     return mtx
+
+def modularity(cluster, mtx):
+    chromosomeLen = mtx.shape[0]
+    edge = np.count_nonzero(mtx == 1) / 2
+    clusterNum = np.unique(cluster).shape[0]
+
+    Qvalue = 0
+    for c in range(1, clusterNum+1):
+        inValue = 0
+        outValue = 0
+        for i in range(chromosomeLen):
+            for j in range(i + 1, chromosomeLen):
+                if cluster[i] == c and cluster[j] == c:
+                    inValue += mtx[i, j]
+                elif cluster[i] != c and cluster[j] == c:
+                    outValue += mtx[i, j]/2
+                elif cluster[j] != c and cluster[i] == c:
+                    outValue += mtx[i, j]/2
+        outValue += inValue
+        Qvalue += inValue / edge
+        Qvalue -= (outValue / edge)**2
+    return Qvalue
+
+def transfer_cluster(cluster):
+    cluster_np = np.zeros(34)
+    clusterNum = 1
+    for subcluster in cluster:
+        for node in subcluster:
+            cluster_np[node] = clusterNum
+        clusterNum += 1
+    return cluster_np
